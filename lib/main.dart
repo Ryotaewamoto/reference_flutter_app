cf20f5f902801d17fdb043606d16b147065c4295
ryotaewamoto <lengtaiyanben@gmail.com>
2023-01-04 00:16:26 +0900

    feat: set up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import '../firebase_options_dev.dart' as dev;
+import '../firebase_options_prod.dart' as prod;
+
+const flavor = String.fromEnvironment('FLAVOR');
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Flavor に応じた FirebaseOptions を準備する
+  final firebaseOptions = flavor == 'prod'
+      ? prod.DefaultFirebaseOptions.currentPlatform
+      : dev.DefaultFirebaseOptions.currentPlatform;
+
+  // Firebase の初期化
+  await Firebase.initializeApp(
+    options: firebaseOptions,
+  );
+
+  // FirebaseUser を取得する
+  final firebaseUser = await FirebaseAuth.instance.userChanges().first;
+  debugPrint('uid = ${firebaseUser?.uid}');
+  if (firebaseUser == null) {
+    // 未サインインなら匿名ユーザーでサインインする
+    final credential = await FirebaseAuth.instance.signInAnonymously();
+    final uid = credential.user!.uid;
+    debugPrint('Signed in: uid = $uid');
+  }
   runApp(const MyApp());
 }
 
